8b376f20f3784d4840b4d8aa25eb68e91846bc4e
Ashish Tele <sale.ashish.tele@gmail.com>
2020-08-25 21:03:36 -0400

    Update Coding_Practice.py
    1 added

--- Coding_Practice.py
+++ Coding_Practice.py
@@ -1,1 +1,8 @@
-New file
+# Two sum
+def  twoSum (nums, target ):
+	 d  = {}
+	 for  i , n  in  enumerate ( nums ):
+	     if  n  in  d : return [ d [ n ], i ]
+	     d [ target - n ]=  i
+
+twoSum([3,2,4],6)
